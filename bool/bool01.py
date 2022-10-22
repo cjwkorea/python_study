7e53833472e6656a126398be7f001286cb9b7ba1
cjwkorea <cjwkorea0@gmail.com>
2022-10-22 13:47:45 +0900

    list 정리

---
+++ bool/bool01.py
@@ -0,0 +1,17 @@
+temp = True
+temp = False
+temp = 10 > 1
+temp = "가나다".startswith("가")
+
+# if temp :
+#     print("")
+
+
+temp = None
+# None = data 없음
+# null, nil 데이터 없음
+if [1 , 2] :
+    print("?")
+else:
+    print("!")
+# ?
---
+++ list/list01.py
@@ -0,0 +1,46 @@
+temp = [1, 2] + [3, 4]
+# [1, 2, 3, 4]
+temp.append(5)
+# [1, 2, 3, 4, 5]
+temp.remove(1)
+# [2, 3, 4, 5]
+temp = [1, 1, 1, 1]
+temp.remove(1)
+# 모든 같은 데이터를 지우지 않는다.
+# 하나만 지운다
+# [1, 1, 1]
+
+
+def check(num):
+    if num == 1:
+        return False
+    else:
+        return True
+
+
+# print(check(1))
+# print(True)
+
+temp = [1, 2, 3, 1, 1]
+temp = filter(check, temp)
+temp = list(temp)
+# [2, 3]
+
+# temp = list(filter(check, [1, 1, 1, 1]))
+
+# print(temp)
+
+temp = [1, 2, 3, 4, 5]
+temp = [[1, 2], [3, 4]]
+
+# print(temp[0])
+# [1 ,2]
+# print(temp[0][0])
+# 1
+
+temp = [(1, 2), (3, 4)]
+temp = [{"key": "value", "name": "이름"}, {"b": "d", "aa": "dd"}]
+print(temp[0])
+# {'key': 'value', 'name': '이름'}
+print(temp[0]["name"])
+# 이름

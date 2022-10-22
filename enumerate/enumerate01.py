a641b63d74bbe3fa496a06d939f667b7433bf88d
cjwkorea <cjwkorea0@gmail.com>
2022-10-22 14:24:02 +0900

    enumerate

---
+++ enumerate/enumerate01.py
@@ -0,0 +1,11 @@
+temp = {"a", "b", "c"}
+# 랜덤으로 abc가 나온다
+temp = enumerate(temp)
+# 랜덤으로 (0,(a,b,c))가 나온다
+# for num, value in temp:
+# print(num, value)
+
+a, b = {"name": "이름", "age": 12}.items()
+
+print(a) 
+print(b)
